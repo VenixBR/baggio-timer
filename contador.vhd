91157063f2063e19fb09f2eb2b0821ab41cfe14b
viniciusschultz <148165925+VenixBR@users.noreply.github.com>
2024-12-06 10:54:43 -0300

    Update contador.vhd

--- contador.vhd
+++ contador.vhd
@@ -3,12 +3,17 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 
 entity contador is
-	Port(
-		clock : in STD_LOGIC;
-		d0 : out STD_LOGIC_VECTOR (6 downto 0) := "0000001";
-		selector : out STD_LOGIC_VECTOR(3 downto 0) := "1110"
-	);
-
+    Port(
+        clock : in STD_LOGIC;
+        d0 : out STD_LOGIC_VECTOR (6 downto 0) := "0000001";  -- Display
+        selector : out STD_LOGIC_VECTOR(3 downto 0) := "1110";  -- Controla quais displays são ativados
+        botaoA : in STD_LOGIC;  -- Botão A
+        botaoB : in STD_LOGIC;  -- Botão B
+        botaoC : in STD_LOGIC;  -- Botão C
+        botaoD : in STD_LOGIC;  -- Botão D
+        botaoE : in STD_LOGIC;  -- Botão E
+        led : out STD_LOGIC  -- LED de controle
+    );
 end contador;
 
 --100Mhz
@@ -19,10 +24,10 @@ end contador;
 architecture Behavioral of contador is
 
 	signal contador : integer range 0 to 100e4 := 0;
-	signal segundo1 : integer range 0 to 10 := 0;
-	signal segundo0 : integer range 0 to 10 := 0;
-	signal decimo : integer range 0 to 10 := 0;
-	signal centesimo : integer range 0 to 10 := 0;
+	signal segundo1 : integer range 0 to 0 := 0;
+	signal segundo0 : integer range 0 to 12 := 0;
+	signal decimo : integer range 0 to 9 := 0;
+	signal centesimo : integer range 0 to 9 := 0;
 	signal conta_ate4 : integer range 0 to 4 := 0;
 	signal entrada : integer range 0 to 9 := 0;
 	signal clk100hz : STD_LOGIC := '0';
@@ -46,23 +51,45 @@ begin
 		if rising_edge(clock) then
 			if contador=100e4 then
 				contador <= 0;
-
-				if centesimo=10 then
+				
+				if centesimo=9 then
 					centesimo <= 0;
 
-						if decimo=10 then
+						if decimo=9 then
 							decimo <= 0;
 
-							if segundo0=10 then
+							if segundo0=9 then
 								segundo0 <= 0;
 
-								if segundo1=10 then
+								if segundo1=9 then
 									segundo1 <= 0;
 								else
-									segundo1 <= segundo1 +1;
+									segundo1 <= segundo1 + 1;
 								end if;
 							else
-								segundo0 <= segundo0 + 1;
+								-- Verifica se botao A ou C esta pressionado
+								if botaoA = '1' then
+									segundo0 <= segundo0 + 3;
+									if segundo0 > 6 then
+										segundo0 <= segundo0 mod 10;
+										segundo1 <= segundo1 + 1;
+									end if;
+									
+								elsif botaoC = '1' then
+									if segundo0 = 0 then
+										if segundo1 = 0 then
+											led <= '1';
+										else
+											segundo0 <= 9;
+											segundo1 <= segundo1 - 1;
+										end if;
+									else
+										segundo0 <= segundo0 - 1;
+									end if;
+								else
+									led <= '0';
+									segundo0 <= segundo0 + 1;
+								end if;
 							end if;
 						else
 							decimo <= decimo+1;
@@ -80,7 +107,7 @@ begin
 	process(clock)
 	begin
 		if rising_edge(clock) then
-			if cont = 10e5 then
+			if cont = 10e3 then
 				cont <= 0;
 				clk100hz <= not clk100hz;
 			else
@@ -114,5 +141,3 @@ begin
 	end process;
 
 end Behavioral;
-
-
